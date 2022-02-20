fdbca01512ce179ae670b2939048518bdb67e301
Francisco C J Bispo <fcjbispo@gmail.com>
2022-02-20 19:19:59 -0300

    Version 1.3.0; add support to update xlsx files;

--- fbpyutils/xlsx.py
+++ fbpyutils/xlsx.py
@@ -3,18 +3,14 @@ Functions to read and MS Excel Spreadsheet files in xls or xlsx formats.
 '''
 import os
 import io
-from click.termui import prompt
 import openpyxl, xlrd
-import csv
-import json
-import click
+from openpyxl import load_workbook
 
-from datetime import datetime, date
 import pandas as pd
-import numpy as np
 
 from typing import Union
 
+import warnings
 
 XLS, XLSX = 0, 1
 
@@ -43,6 +39,7 @@ class ExcelWorkbook:
 
         try:
             xl_data = io.BytesIO(data)
+            warnings.simplefilter("ignore")
             self.workbook = openpyxl.open(xl_data, data_only=True)
             self.sheet_names = self.workbook.sheetnames
         except OSError:
@@ -50,6 +47,8 @@ class ExcelWorkbook:
             self.workbook = xlrd.open_workbook(file_contents=xl_data.read())
             self.sheet_names = self.workbook.sheet_names()
             self.kind = XLS
+        finally:
+            warnings.simplefilter("default")
 
     def read_sheet(self, sheet_name=None):
         sheet_name = sheet_name or self.sheet_names.get(sheet_name)
@@ -82,3 +81,29 @@ def get_all_sheets(xl_file):
     xl = ExcelWorkbook(xl_file)
     sheet_names = xl.sheet_names
     return {sheet_name: tuple(xl.read_sheet(sheet_name)) for sheet_name in sheet_names}
+
+
+def write_to_sheet(df, workbook_path, sheet_name):
+    if not os.path.exists(workbook_path):
+        df.to_excel(
+            workbook_path, sheet_name=sheet_name, index=False, freeze_panes=(1,0), encoding='utf-8', header=True)
+    else:
+        warnings.simplefilter("ignore")
+        book = load_workbook(workbook_path)
+        writer = pd.ExcelWriter(workbook_path, engine='openpyxl') 
+        writer.book = book
+
+        writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+
+        index, sheet_names = 0, [ws.title for ws in book.worksheets]
+        while sheet_name in sheet_names:
+            index += 1
+            sheet_name = sheet_name + str(index)
+
+        df.to_excel(
+            writer, sheet_name=sheet_name, index=False, freeze_panes=(1,0), encoding='utf-8', header=True)
+
+        writer.save()
+        writer.close()
+        book.close()
+        warnings.simplefilter("default")
